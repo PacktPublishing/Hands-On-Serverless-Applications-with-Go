2a3a32473722608ce25f75d64bc63e98fe12f879
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2018-07-21 18:39:22 +0200

    create custom sub-segment

--- ch11/parser/main.go
+++ ch11/parser/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/PuerkitoBio/goquery"
@@ -10,7 +10,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-xray-sdk-go/xray"
 	uuid "github.com/satori/go.uuid"
+	"golang.org/x/net/context/ctxhttp"
 )
 
 // Movie entity
@@ -21,35 +23,50 @@ type Movie struct {
 	Description string `json:"description"`
 }
 
-func handler() (Movie, error) {
+func handler(ctx context.Context, url string) (Movie, error) {
+	xray.Configure(xray.Config{
+		LogLevel:       "info",
+		ServiceVersion: "1.2.3",
+	})
+
 	// Get html page
-	res, err := http.Get("https://www.themoviedb.org/movie/351286-jurassic-world-fallen-kingdom")
+	res, err := ctxhttp.Get(ctx, xray.Client(nil), url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 
 	// Parse HTML response
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	movie := Movie{}
+	xray.Capture(ctx, "Parsing", func(ctx1 context.Context) error {
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			return err
+		}
 
-	title := doc.Find(".header .title span a h2").Text()
-	description := doc.Find(".overview p").Text()
-	cover, _ := doc.Find(".poster .image_content img").Attr("src")
+		title := doc.Find(".header .title span a h2").Text()
+		description := doc.Find(".overview p").Text()
+		cover, _ := doc.Find(".poster .image_content img").Attr("src")
 
-	movie := Movie{
-		ID:          uuid.Must(uuid.NewV4()).String(),
-		Name:        title,
-		Description: description,
-		Cover:       cover,
-	}
+		movie = Movie{
+			ID:          uuid.Must(uuid.NewV4()).String(),
+			Name:        title,
+			Description: description,
+			Cover:       cover,
+		}
+
+		xray.AddMetadata(ctx1, "movie.title", title)
+		xray.AddMetadata(ctx1, "movie.description", description)
+		xray.AddMetadata(ctx1, "movie.cover", cover)
+
+		return nil
+	})
 
 	// Save in dynamodb
 	sess := session.Must(session.NewSession())
 	dynamo := dynamodb.New(sess)
-	req, _ := dynamo.PutItemRequest(&dynamodb.PutItemInput{
+	xray.AWS(dynamo.Client)
+	_, err = dynamo.PutItemWithContext(ctx, &dynamodb.PutItemInput{
 		TableName: aws.String(os.Getenv("TABLE_NAME")),
 		Item: map[string]*dynamodb.AttributeValue{
 			"ID": &dynamodb.AttributeValue{
@@ -66,7 +83,6 @@ func handler() (Movie, error) {
 			},
 		},
 	})
-	err = req.Send()
 	if err != nil {
 		log.Fatal(err)
 	}
