11e4ea77143a4812c922b8914be4d01d4f34791c
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2018-07-29 17:19:04 +0200

    decrypt using KMS

--- ch12/encryption/main.go
+++ ch12/encryption/main.go
@@ -2,22 +2,47 @@ package main
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler() error {
-	MYSQL_USERNAME := os.Getenv("MYSQL_USERNAME")
-	MYSQL_PASSWORD := os.Getenv("MYSQL_PASSWORD")
-	MYSQL_DATABASE := os.Getenv("MYSQL_DATABASE")
-	MYSQL_PORT := os.Getenv("MYSQL_PORT")
-	MYSQL_HOST := os.Getenv("MYSQL_HOST")
+var encryptedMysqlUsername string = os.Getenv("MYSQL_USERNAME")
+var encryptedMysqlPassword string = os.Getenv("MYSQL_PASSWORD")
+var mysqlDatabase string = os.Getenv("MYSQL_DATABASE")
+var mysqlPort string = os.Getenv("MYSQL_PORT")
+var mysqlHost string = os.Getenv("MYSQL_HOST")
+var decryptedMysqlUsername, decryptedMysqlPassword string
 
-	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE)
+func decrypt(encrypted string) (string, error) {
+	kmsClient := kms.New(session.New())
+	decodedBytes, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return "", err
+	}
+	input := &kms.DecryptInput{
+		CiphertextBlob: decodedBytes,
+	}
+	response, err := kmsClient.Decrypt(input)
+	if err != nil {
+		return "", err
+	}
+	return string(response.Plaintext[:]), nil
+}
+
+func init() {
+	decryptedMysqlUsername, _ = decrypt(encryptedMysqlUsername)
+	decryptedMysqlPassword, _ = decrypt(encryptedMysqlPassword)
+}
+
+func handler() error {
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", decryptedMysqlUsername, decryptedMysqlPassword, mysqlHost, mysqlPort, mysqlDatabase)
 	db, err := sql.Open("mysql", uri)
 	if err != nil {
 		return err
