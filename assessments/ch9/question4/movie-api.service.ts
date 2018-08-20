5b8adbe2b1dfc9f15979b6ae162f270d2b8ff218
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2018-08-21 00:24:30 +0100

    update an existing item with dynamodb

---
+++ assessments/ch9/question4/movie-api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, Inject } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
+import { Movie } from '../models/movie';
+import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
+
+@Injectable()
+export class MoviesApiService {
+
+  constructor(private http:Http,
+              @Inject(LOCAL_STORAGE) private storage: WebStorageService) { }
+
+  findAll(){
+    return this.http
+      .get(environment.api, {headers: this.getHeaders()})
+      .map(res => {
+        return res.json()
+      })
+  }
+
+  insert(movie: Movie){
+    return this.http
+      .post(environment.api, JSON.stringify(movie), {headers: this.getHeaders()})
+      .map(res => {
+        return res
+      })
+  }
+
+  update(movie: Movie){
+    return this.http
+      .put(environment.api, JSON.stringify(movie), {headers: this.getHeaders()})
+      .map(res => {
+        return res
+      })
+  }
+
+  getHeaders(){
+    let headers = new Headers()
+    headers.append('Authorization', this.storage.get("COGNITO_TOKEN"))
+    return headers
+  }
+}
