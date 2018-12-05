8dabb384c47e02b696f8d6b1bee282de8cb42c62
Omar Khawaja <khawaja910@gmail.com>
2018-12-05 17:21:27 -0500

    add Headers and IsBase64Encoded fields to Response struct

--- ch4/version1/main.go
+++ ch4/version1/main.go
@@ -5,8 +5,10 @@ import (
 )
 
 type Response struct {
-	StatusCode int    `json:"statusCode"`
-	Body       string `json:"body"`
+	StatusCode      int               `json:"statusCode"`
+	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+	IsBase64Encoded bool              `json:"isBase64Encoded,omitempty"`
 }
 
 func handler() (Response, error) {
